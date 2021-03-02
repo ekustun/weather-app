94b48e7e1ccc322f3c676f19eb5331f06be9ffa0
ekustun <ekaderustun@gmail.com>
2021-03-02 13:37:15 -0800

    weather app - week5

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+// Constants and variables
+const API_KEY = '7131d2933a8741dce1c595695162ed19';
+const BASE_URL ='https://api.openweathermap.org/data/2.5/weather?';
+
+//?q=pasadena&appid=abc123$units=imperial
+
+let weatherData, userInput;
+
+const $cityname = $('#cityName');
+const $temp = $('#temp');
+const $feelslike = $('#feelsLike');
+const $weather = $('#weather');
+
+const $input = $('input[type="text"]');
+
+
+
+$('form').on('submit', handleGetData);
+
+// '&units=imperial'
+function handleGetData(event){
+
+    event.preventDefault();
+    userInput = $input.value= '';
+  
+    $.ajax(BASE_URL + 'q='+ userInput + '&appid=' + API_KEY )
+    .then(
+        (data)=>{
+            weatherData = data;
+            render();
+        },
+        (error)=>{
+            console.log("Error: ", error);
+        }
+    )
+   
+    
+
+}
+
+
+function render(){
+    $cityname.text(weatherData.name);
+    $temp.text(weatherData.main.temp);
+    $feelslike.text(weatherData.main.feels_like);
+    $weather.text(weatherData.weather[0].description);
+}
